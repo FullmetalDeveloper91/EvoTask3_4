fcb9a6b2d50821195958bd9c8f2b3366530a03bc
andrei_chudotvorov <lonelywolf_91@mail.ru>
2025-02-28 15:10:35 +0700

    Task3_4 is complete

--- src/main/java/ru/fmd/Main.java
+++ src/main/java/ru/fmd/Main.java
@@ -1,7 +1,27 @@
 package ru.fmd;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String name;
+        int age;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите имя первого пользователя");
+        name = scanner.nextLine();
+        System.out.println("Введите возраст первого пользователя");
+        age = scanner.nextInt();
+        User user1 = new User(name, age);
+
+        scanner.nextLine();
+
+        System.out.println("Введите имя второго пользователя");
+        name = scanner.nextLine();
+        System.out.println("Введите возраст второго пользователя");
+        age = scanner.nextInt();
+        User user2 = new User(name, age);
+
+        System.out.println( user1.getAge() < user2.getAge()? user1 : user2 );
     }
 }
